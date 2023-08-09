68a65511b362db0df936fbad4d66859fdc768d80
PoojaMathumitha <141608682+PoojaMathumitha@users.noreply.github.com>
2023-08-09 13:37:13 +0530

    Delete positive_negative_zero.py

--- positive_negative_zero.py
+++
@@ -1,8 +0,0 @@
-#check positive ,negative or zero
-num=int(input("enter a number:"))
-if num<0:
-    print(num,"negative number")
-elif num>0:
-    print(num,"positive number")
-else:
-    print(num,"number is zero")
